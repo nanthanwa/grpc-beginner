69b1b1fb4750b8ff5e69a87b0f9256d647627303
Thanwa JDR <thanwa.jdr@gmail.com>
2018-10-09 17:20:19 +0700

    deadline-completed

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -7,6 +7,9 @@ import (
 	"log"
 	"time"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/simplesteph/grpc-go-course/greet/greetpb"
 
 	"google.golang.org/grpc"
@@ -27,7 +30,11 @@ func main() {
 	// doServerStreaming(c)
 
 	// doClientStreaming(c)
-	doBiDiStreaming(c)
+
+	// doBiDiStreaming(c)
+
+	doUnaryWithDeadline(c, "Thanwa", 1*time.Second)
+	doUnaryWithDeadline(c, "Nick", 5*time.Second)
 
 }
 
@@ -183,4 +190,32 @@ func doBiDiStreaming(c greetpb.GreetServiceClient) {
 	// block until everthing is done
 	<-waitc
 
+}
+
+func doUnaryWithDeadline(c greetpb.GreetServiceClient, firstName string, timeout time.Duration) {
+	fmt.Println("Starting to do a UnaryWithDeadline RPC...")
+	req := &greetpb.GreetWithDeadlineRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: firstName,
+			LastName:  "JDR",
+		},
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	res, err := c.GreetWithDeadline(ctx, req)
+	if err != nil {
+		statusErr, ok := status.FromError(err)
+		if ok {
+			if statusErr.Code() == codes.DeadlineExceeded {
+				fmt.Println("Timeout was hit! Deadline was exceeded")
+			} else {
+				fmt.Printf("Unexpected error: %v", statusErr)
+			}
+		} else {
+			log.Fatalf("error while calling UnaryWithDeadline RPC: %v", err)
+		}
+		return
+	}
+	log.Printf("Response from Greet: %v", res.Result)
 }
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -9,6 +9,9 @@ import (
 	"strconv"
 	"time"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/simplesteph/grpc-go-course/greet/greetpb"
 	"google.golang.org/grpc"
 )
@@ -82,6 +85,30 @@ func (*server) GreetEveryone(stream greetpb.GreetService_GreetEveryoneServer) er
 			return sendErr
 		}
 	}
+}
+
+func (*server) GreetWithDeadline(ctx context.Context, req *greetpb.GreetWithDeadlineRequest) (*greetpb.GreetWithDeadlineResponse, error) {
+	fmt.Printf("GreetWithDeadline function was invoked with %v\n", req)
+	// for i := 0; i < 3; i++ {
+	// 	if ctx.Err() == context.Canceled {
+	// 		// the cliemt canceled the request
+	// 		fmt.Println("The client canceled the request!")
+	// 		return nil, status.Error(codes.Canceled, "the client canceled the request")
+	// 	}
+	// 	time.Sleep(1 * time.Second)
+	// }
+	if ctx.Err() == context.Canceled {
+		// the cliemt canceled the request
+		fmt.Println("The client canceled the request!")
+		return nil, status.Error(codes.Canceled, "the client canceled the request")
+	}
+	time.Sleep(3 * time.Second)
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetWithDeadlineResponse{
+		Result: result,
+	}
+	return res, nil
 }
 
 func main() {
